f3c277f91a34955ac29532796d9c1b13f8442de8
Nuri Kim <kimnooree@cttd.co.kr>
2024-04-16 15:51:58 +0900

    wip

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -9,7 +9,7 @@ const routes = {
 },
 };
 
-export const pug = () => gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
+ const pug = () => gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
 
 const clean = () => del(["build"]);
 
@@ -19,7 +19,9 @@ const assets = gulp.series([pug]);
 
 export const dev = gulp.series([prepare, assets]);
 
-//gulp.src() : gulp 작업 타겟들의 경로 및 형식을 지정!
+// gulp.src() : gulp 작업 타겟들의 경로 및 형식을 지정!
 // gulp.pipe() : pipe를 통과시키면서 추가 작업을 함!
 // gulp.series() : 여러 task 를 전달받아 직렬 실행해주는 메소드
 // gulp.dest("폴더명") : 여기에 결과물을 생성!
+// export는 package.json에서 쓸 command만 해주면 됨. 
+// 만약 clean을 export 하지 않는다면, console 이나 package.json에서 사용하지 못함.
