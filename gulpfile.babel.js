caecc8e15d879b9251b191ed58dc6fdbc1227004
Nuri Kim <kimnooree@cttd.co.kr>
2024-04-17 15:45:00 +0900

    gulp watch 추가 (gul-webserver)

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,9 +1,11 @@
 import gulp from "gulp";
 import gpug from "gulp-pug";
 import del from "del";
+import ws from "gulp-webserver";
 
 const routes = {
   pug: {
+    watch: "src/**/*.pug",
     src:"src/*.pug",
     dest: "build",
 },
@@ -11,13 +13,25 @@ const routes = {
 
  const pug = () => gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
 
-const clean = () => del(["build"]);
+const clean = () => del(["build/"]);
+
+const webserver = () => gulp.src("build").pipe(ws({
+  livereload: true, open: true
+}));
+
+const watch = () => {
+  gulp.watch(routes.pug.watch, pug);
+}
 
 const prepare = gulp.series([clean]);
 
 const assets = gulp.series([pug]);
 
-export const dev = gulp.series([prepare, assets]);
+//두가지 task를 병행하게 함.
+const postDev = gulp.parallel([webserver, watch]);
+
+//gulp series를 실행할 때 마다 prepare, assets, postDev를 실행한다.
+export const dev = gulp.series([prepare, assets, postDev]);
 
 // gulp.src() : gulp 작업 타겟들의 경로 및 형식을 지정!
 // gulp.pipe() : pipe를 통과시키면서 추가 작업을 함!
